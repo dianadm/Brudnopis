309ba4d567e6c6dcc8404bc4f1bd01d52dd3a616
Diana Migasiuk <diana.migasiuk@contractors.roche.com>
2018-01-12 19:00:52 +0100

    Squashed commit

---
+++ src/knowledge/second/GitTestingClass.java
@@ -0,0 +1,11 @@
+package knowledge.second;
+
+
+public class GitTestingClass {
+
+    public static void main(String[] args) {
+        System.out.println("Helllooooo");
+        System.out.println("No heja!");
+        System.out.println("Siemano");
+    }
+}
