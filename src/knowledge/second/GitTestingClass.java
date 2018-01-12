f83ca3fb5b57214577ffb5d07826699dccb7192d
Diana Migasiuk <diana.migasiuk@contractors.roche.com>
2018-01-12 18:55:35 +0100

    test commit 3

--- src/knowledge/second/GitTestingClass.java
+++ src/knowledge/second/GitTestingClass.java
@@ -6,5 +6,6 @@ public class GitTestingClass {
     public static void main(String[] args) {
         System.out.println("Helllooooo");
         System.out.println("No heja!");
+        System.out.println("Siemano");
     }
 }
