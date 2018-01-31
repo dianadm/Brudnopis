e89948fa0016f012e892421f8dd4325fe4f94d58
Diana Migasiuk <diana.migasiuk@contractors.roche.com>
2018-01-31 14:41:13 +0100

    strings

--- src/knowledge/third/StringPool.java
+++ src/knowledge/third/StringPool.java
@@ -39,24 +39,54 @@ public class StringPool {
 
     }
 
-    public void testStringBuilder() {
+    public static void testStringBuilder() {
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder(10);
         // capacity and size
+        System.out.println(sb1.capacity());
+        System.out.println(sb1.length());
+        System.out.println(sb2.capacity());
+        System.out.println(sb2.length());
 
         StringBuilder sb = new StringBuilder("ABCD");
+        System.out.println(sb.length());
 
         sb.charAt(1); sb.indexOf("A"); sb.length(); sb.substring(1); // the same as string
+        System.out.println(sb);
 
-        sb.append("ZZZ");
+        System.out.println(sb.append("ZZZ"));
 
         sb.insert(1, "XXX");
+        System.out.println(sb);
 
-        sb.delete(1, 2);
+        sb.delete(4, 6);
+        System.out.println(sb);
+
         sb.deleteCharAt(2);
+        System.out.println(sb);
 
         sb.reverse();
+        System.out.println(sb);
 
         sb.toString();
+    }
+
+    public static void main(String[] args) {
+        testStringBuilder();
+
+        String testing = "ABCD";
+        System.out.println("---------------");
+        System.out.println(testing.charAt(1));
+        System.out.println(testing.indexOf('A'));
+        System.out.println(testing.indexOf('A', -1));
+        System.out.println(testing.indexOf("A"));
+        System.out.println(testing.indexOf("A", 0));
+        System.out.println(testing.substring(2));
+        System.out.println(testing.substring(1, 4));
+        System.out.println(testing.equals("abcd"));
+        System.out.println(testing.equalsIgnoreCase("abcd"));
+        System.out.println(testing.replace('A', 'Z'));
+        System.out.println(testing.replace("A", "Z"));
+        System.out.println(testing.substring(1,3).toLowerCase().replace("b", "AA"));
     }
 }
